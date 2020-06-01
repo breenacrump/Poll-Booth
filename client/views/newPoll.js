28dda441c8771379c0936b1861944aa4e03a67ec
Breena Crump <breena.crump@ge.com>
2020-06-01 10:30:56 -0500

    Added logic to make poll title required

--- client/views/newPoll.js
+++ client/views/newPoll.js
@@ -1,6 +1,7 @@
 Template.newPoll.created = function(){
 	var template = this;
 	template.creatingPoll = new ReactiveVar(false);
+	template.invalid = new ReactiveVar(false);
 };
 
 Template.newPoll.events = {
@@ -8,7 +9,8 @@ Template.newPoll.events = {
 		event.preventDefault();
 		var template = Template.instance();
 		template.creatingPoll.set(true);
-	},
+        template.invalid.set(false);
+    },
 	'click button[data-action="cancel-new-poll"]': function(event){
 		event.preventDefault();
 		var template = Template.instance();
@@ -17,16 +19,26 @@ Template.newPoll.events = {
 	'click button[data-action="create-new-poll"]': function(event){
 		event.preventDefault();
 		var template = Template.instance();
-		Polls.insert({
-			userId: Meteor.userId(),
-			title: template.$('#newPollTitle').val(),
-			description: template.$('#newPollDescription').val(),
-			timestamp: moment().valueOf(),
-			options: [
-				'Yes',
-				'No'
-			]
-		});
-		template.creatingPoll.set(false);
+		console.log('title', !!template.$('#newPollTitle').val());
+		// If the poll has a title then allow the insert
+		if (template.$('#newPollTitle').val()) {
+			console.log('valid poll insert');
+            Polls.insert({
+            	userId: Meteor.userId(),
+            	title: template.$('#newPollTitle').val(),
+            	description: template.$('#newPollDescription').val(),
+            	timestamp: moment().valueOf(),
+            	options: [
+            		'Yes',
+            		'No'
+            	]
+            });
+            template.creatingPoll.set(false);
+		} else {
+			// Else inform the user that the poll does not have a title
+			console.log('invalid poll insert');
+            template.$('#newPollTitle').prop('required', true);
+            template.invalid.set(true);
+		}
 	}
 };
