5104aa3498308251dcd9231e365bde067ec82498
Breena Crump <breena.crump@ge.com>
2020-06-01 01:08:27 -0500

    Added logic to only let users that are authenticated to vote and create polls on the client and server side. Added logic to only let users vote once

--- client/views/poll.js
+++ client/views/poll.js
@@ -1,3 +1,9 @@
+Template.pollDetails.created = function(){
+    var template = this;
+    template.isSubmitted = new ReactiveVar(false);
+    template.hasSubmissionError = new ReactiveVar(false);
+};
+
 Template.pollListItem.events = {
 	'click button[data-action="view-poll"]': function(event){
 		event.preventDefault();
@@ -8,20 +14,38 @@ Template.pollListItem.events = {
 Template.pollDetails.events = {
 	'click button[data-action="vote-on-poll"]': function(event){
 		event.preventDefault();
+		var template = Template.instance();
 		var poll = Template.currentData();
 		var pollOption = this;
+
+		//If I want to let the user see the polls but just stop them from voting
+		// if (!Meteor.userId()){
+		// 	throw new Meteor.Error('not-authorized');
+		// }
+
+		console.log('poll', Votes.findOne({userId: Meteor.userId()}));
+
 		Votes.insert({
 			userId: Meteor.userId(),
 			pollId: poll && poll._id,
 			timestamp: moment().valueOf(),
 			option: pollOption.valueOf()
-		});
-	}
+		}, function (err) {
+			if (err) {
+                template.hasSubmissionError.set(true);
+			} else {
+                template.isSubmitted.set(true);
+            }
+        });
+    }
 };
 
 Template.pollDetails.helpers({
 	'voteCountForOption': function(){
 		var pollOption = this;
 		return Votes.find({option: pollOption.valueOf()}).count();
-	}
+	},
+    'hasUserVoted': function () {
+        return Votes.findOne({userId: Meteor.userId()});
+    }
 });
--- server/collectionPermissions.js
+++ server/collectionPermissions.js
@@ -1,23 +1,26 @@
+// Only allow if the user is logged in they can perform these actions
 Polls.allow({
 	insert: function(userId, doc){
-		return true;
+		return userId;
 	},
 	update: function(userId, doc, fields, modifier){
-		return true;
+        return userId;
 	},
 	remove: function(userId, doc){
-		return true;
+        return userId;
 	}
 });
 
+// Only allow if the user is logged in they can perform these actions
 Votes.allow({
 	insert: function(userId, doc){
-		return true;
-	},
+		// Only vote if user is logged in AND the user has not voted yet
+		return userId && !Votes.findOne({userId: userId});
+    },
 	update: function(userId, doc, fields, modifier){
-		return true;
+		return userId;
 	},
 	remove: function(userId, doc){
-		return true;
+		return userId;
 	}
 });
